d1a645a8c3149fa15db4ee929a8d3782985bac72
Kaplas <kaplas80@gmail.com>
2022-08-24 16:36:43 +0200

    Add tests for new methods

---
+++ src/Tests/TF3.Tests/Converters/BitmapImage/ExtractToPngTests.cs
@@ -0,0 +1,56 @@
+﻿namespace TF3.Tests.Converters.BitmapImage
+{
+    using System;
+    using NUnit.Framework;
+    using TF3.Core.Converters.BitmapImage;
+    using TF3.Core.Converters.Common;
+    using TF3.Core.Formats;
+    using Yarhl.IO;
+
+    public class ExtractToPngTests
+    {
+        private readonly byte[] _validData = { 0x01 };
+
+        [Test]
+        public void NullSourceThrowsException()
+        {
+            var converter = new Extractor();
+            converter.Initialize(new ImageExtractorParameters());
+            _ = Assert.Throws<ArgumentNullException>(() => converter.Convert(null));
+        }
+
+        [Test]
+        public void NullImageThrowsException()
+        {
+            var converter = new Extractor();
+            converter.Initialize(new ImageExtractorParameters());
+            var format = new BitmapFileFormat();
+            _ = Assert.Throws<NullReferenceException>(() => converter.Convert(format));
+        }
+
+        [Test]
+        public void Export()
+        {
+            using DataStream ds = DataStreamFactory.FromArray(_validData, 0, _validData.Length);
+            using var format = new BinaryFormat(ds);
+
+            var reader = new Reader();
+
+            reader.Initialize(new ImageParameters { PixelFormat = Core.Enums.BitmapPixelFormat.A8, ImageWidth = 1, ImageHeight = 1 });
+            BitmapFileFormat bmp = reader.Convert(format);
+
+            var converter = new Extractor();
+            converter.Initialize(new ImageExtractorParameters());
+
+            BinaryFormat result = converter.Convert(bmp);
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Stream);
+
+            result.Stream.Position = 0;
+            var reader1 = new DataReader(result.Stream);
+            ulong magic = reader1.ReadUInt64();
+
+            Assert.AreEqual(727905341920923785, magic);
+        }
+    }
+}
---
+++ src/Tests/TF3.Tests/Converters/BitmapImage/ExtractToTgaTests.cs
@@ -0,0 +1,70 @@
+﻿namespace TF3.Tests.Converters.BitmapImage
+{
+    using System;
+    using BCnEncoder.Shared.ImageFiles;
+    using NUnit.Framework;
+    using TF3.Core.Converters.BitmapImage;
+    using TF3.Core.Converters.Common;
+    using TF3.Core.Enums;
+    using TF3.Core.Formats;
+    using Yarhl.IO;
+
+    public class ExtractToTgaTests
+    {
+        private readonly byte[] _validData = { 0x01 };
+
+        [Test]
+        public void NullSourceThrowsException()
+        {
+            var converter = new Extractor();
+            converter.Initialize(new ImageExtractorParameters { ImageFormat = BitmapExtractionFormat.Tga });
+            _ = Assert.Throws<ArgumentNullException>(() => converter.Convert(null));
+        }
+
+        [Test]
+        public void NullImageThrowsException()
+        {
+            var converter = new Extractor();
+            converter.Initialize(new ImageExtractorParameters { ImageFormat = BitmapExtractionFormat.Tga });
+            var format = new BitmapFileFormat();
+            _ = Assert.Throws<NullReferenceException>(() => converter.Convert(format));
+        }
+
+        [Test]
+        public void UnknownFormatThrowsException()
+        {
+            using DataStream ds = DataStreamFactory.FromArray(_validData, 0, _validData.Length);
+            using var format = new BinaryFormat(ds);
+
+            var reader = new Reader();
+
+            reader.Initialize(new ImageParameters { PixelFormat = BitmapPixelFormat.A8, ImageWidth = 1, ImageHeight = 1 });
+            BitmapFileFormat bmp = reader.Convert(format);
+
+            var converter = new Extractor();
+            converter.Initialize(new ImageExtractorParameters { ImageFormat = (BitmapExtractionFormat)4 });
+            _ = Assert.Throws<FormatException>(() => converter.Convert(bmp));
+        }
+
+        [Test]
+        public void Export()
+        {
+            using DataStream ds = DataStreamFactory.FromArray(_validData, 0, _validData.Length);
+            using var format = new BinaryFormat(ds);
+
+            var reader = new Reader();
+
+            reader.Initialize(new ImageParameters { PixelFormat = BitmapPixelFormat.A8, ImageWidth = 1, ImageHeight = 1 });
+            BitmapFileFormat bmp = reader.Convert(format);
+
+            var converter = new Extractor();
+            converter.Initialize(new ImageExtractorParameters { ImageFormat = BitmapExtractionFormat.Tga });
+
+            // TGA files do not have any magic number, so we can only check if no exception has been thrown
+            BinaryFormat result = null;
+            Assert.DoesNotThrow(() => result = converter.Convert(bmp));
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Stream);
+        }
+    }
+}
