5c714ef3ee5b4e92a84d34afc002baa7b18944e0
Kaplas <kaplas80@gmail.com>
2022-09-02 17:13:34 +0200

    Fix tests

--- src/Tests/TF3.Tests/GameScriptTests.cs
+++ src/Tests/TF3.Tests/GameScriptTests.cs
@@ -1,13 +1,32 @@
+// Copyright (c) 2022 Kaplas
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 namespace TF3.Tests
 {
     using System.IO;
-    using System.Text.Json;
     using NUnit.Framework;
 
     public class GameScriptTests
     {
-        private const string TestScript = "{\"Name\":\"test-script\",\"Game\":\"Test Script\",\"Parameters\":[],\"Containers\":[],\"Assets\":[],\"Patches\":[]}";
-        private const string InvalidTestScript = "{\"Name\":\"test-script\",\"Game_#$\":\"Test Script\",\"Parameters\":[],\"Containers\":[],\"Assets\":[],\"Patches\":[]}";
+        private const string TestScript = /*lang=json,strict*/ "{\"Name\":\"test-script\",\"Game\":\"Test Script\",\"Parameters\":[],\"Containers\":[],\"Assets\":[],\"Patches\":[]}";
+        private const string InvalidTestScript = /*lang=json,strict*/ "{\"Name\":\"test-script\",\"Game_#$\":\"Test Script\",\"Parameters\":[],\"Containers\":[],\"Assets\":[],\"Patches\":[]}";
 
         private readonly string _tempPath = Path.Combine(Path.GetTempPath(), "TF3.Tests");
 
@@ -19,7 +38,7 @@ namespace TF3.Tests
                 Directory.Delete(_tempPath, true);
             }
 
-            Directory.CreateDirectory(_tempPath);
+            _ = Directory.CreateDirectory(_tempPath);
             Core.ScriptManager.Clear();
         }
 
@@ -52,13 +71,17 @@ namespace TF3.Tests
         }
 
         [Test]
-        public void PathWithInvalidScriptsThrowsException()
+        public void PathWithInvalidScriptsCallsEvent()
         {
             string scriptPath = Path.Combine(_tempPath, "TF3.Script.Test.json");
             File.WriteAllText(scriptPath, InvalidTestScript);
 
+            int calls = 0;
+            Core.ScriptManager.ErrorLoading += (object _, (string file, string message) _) => calls++;
+
             Assert.AreEqual(0, Core.ScriptManager.Scripts.Count);
-            Assert.Throws<JsonException>(() => Core.ScriptManager.LoadScripts(_tempPath));
+            Core.ScriptManager.LoadScripts(_tempPath);
+            Assert.AreEqual(1, calls);
         }
     }
 }
